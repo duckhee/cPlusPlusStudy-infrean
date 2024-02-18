fa30f965cfb05d85dcced5048a4119b6e7a0cec3
DoukheeWon <fain9301@yahoo.com>
2024-02-18 23:59:44 +0900

    feat: add comment

--- chapter04/ex04-classConstructorDestructor/main.cpp
+++ chapter04/ex04-classConstructorDestructor/main.cpp
@@ -15,14 +15,17 @@ public:
     }
 };
 
+/** main 함수가 호출되기 전에 생성자가 호출이 된다.*/
 Test g_test;
 
 int main() {
     cout << "Begin" << endl;
     Test test;
-    /** 동적으로 할당 */
+    /** 동적으로 할당 -> new 연산이 될 때 생성자가 호출 된다. */
     Test *pTest = new Test();
+    /** 메모리에서 해제가 될 때 소멸자가 호출 된다. */
     delete pTest;
     cout << "end" << endl;
+    /** 호출된 scope 가 종료되는 시점에 소멸자가 호출 된다.*/
     return 0;
 }
