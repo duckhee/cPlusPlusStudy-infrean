11e07666e032536e16b82747264e9f01648ed36e
DoukheeWon <fain9301@yahoo.com>
2024-02-21 22:52:44 +0900

    feat: set copy constructor

---
+++ chapter04/ex11-copyConstructor/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class MyData {
+public:
+    MyData() {
+        cout << "MyData()" << endl;
+    }
+
+    /** 복사 생성자 */
+    MyData(MyData &rhs) : nData(rhs.nData) {
+        cout << "MyData(MyData &rhs)" << endl;
+    }
+
+    void setData(const int data) {
+        this->nData = data;
+    }
+
+    const int getData() {
+        return this->nData;
+    }
+
+private:
+    int nData = 0;
+};
+
+int main() {
+    MyData myData;
+    myData.setData(10);
+    /** 복사 생성자를 이용해서 객체 생성 */
+    MyData copyData(myData);
+    cout << copyData.getData() << endl;
+    return 0;
+}
